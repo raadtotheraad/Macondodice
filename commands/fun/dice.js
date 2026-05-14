abac5db820355a949ebab2ae6dd119ccf78f1fec
Raad <149825178+raadtotheraad@users.noreply.github.com>
2026-05-14 23:02:42 +0200

    Add base code

---
+++ commands/fun/dice.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, ContainerBuilder, MessageFlags } = require('discord.js');
+
+module.exports = {
+    cooldown: 60,
+	data: new SlashCommandBuilder()
+    .setName('dice')
+    .setDescription('Rolls a fairly unusual dice. For some reason, it gets really hot when you roll it.')
+	.addIntegerOption(option =>
+		option.setName('sides')
+			.setDescription('The number of sides on the dice')
+			.setRequired(true)
+			.setMinValue(2)
+			.setMaxValue(64)),
+	async execute(interaction) {
+		const sides = interaction.options.getInteger('sides');
+		const result = Math.floor(Math.random() * sides) + 1;
+		const ContainerSpinning = new ContainerBuilder()
+			.addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent('The dice is rolling!!')
+			),
+		const ContainerResult = new ContainerBuilder()
+			.addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent('The dice has stopped rolling!')
+			)
+			.addSeparatorComponents((separator) => separator)
+			.addTextDisplayComponents((textDisplay) =>
+				textDisplay.setContent(`The result is: ${result}!`)
+			);
+		await interaction.reply({ components: [ContainerSpinning], flags: MessageFlags.IsComponentsV2 });
+		setTimeout(3000)
+		await interaction.editReply({ components: [ContainerResult], flags: MessageFlags.IsComponentsV2 });
+	},
+};
---
+++ index.js
@@ -0,0 +1,84 @@
+// Require the necessary discord.js classes
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { loadEnvFile } = require('node:process');
+loadEnvFile('./.env');
+console.log(process.env.TOKEN)
+const TOKEN = process.env.TOKEN
+const { cooldowns } = interaction.client;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(TOKEN);
+
+client.commands = new Collection(); 
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, (interaction) => {
+	const command = interaction.client.commands.get(interaction.commandName);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		} else {
+			await interaction.reply({
+				content: 'There was an error while executing this command!',
+				flags: MessageFlags.Ephemeral,
+			});
+		}
+	}
+});
+
+client.cooldowns = new Collection();
+if (!cooldowns.has(command.data.name)) {
+	cooldowns.set(command.data.name, new Collection());
+}
+const now = Date.now();
+const timestamps = cooldowns.get(command.data.name);
+const defaultCooldownDuration = 3;
+const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+if (timestamps.has(interaction.user.id)) {
+	const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+	if (now < expirationTime) {
+		const expiredTimestamp = Math.round(expirationTime / 1_000);
+		return interaction.reply({
+			content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+			flags: MessageFlags.Ephemeral,
+		});
+	}
+}
